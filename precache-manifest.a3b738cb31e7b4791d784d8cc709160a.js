5c13100c3233bfb7873fc624c7a42dece66a0781
Mattis Birkhahn <mattisbirkhahn@aol.com>
2024-10-07 00:31:50 +0200

    Updates

---
+++ precache-manifest.a3b738cb31e7b4791d784d8cc709160a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b5518fde7f6ff5d41c2b068f39e580",
+    "url": "/sortly/index.html"
+  },
+  {
+    "revision": "56d9f85dd420f4caa9f7",
+    "url": "/sortly/static/css/main.d5d66f22.chunk.css"
+  },
+  {
+    "revision": "d2d6ebabb0be760a79e7",
+    "url": "/sortly/static/js/2.8de8f01a.chunk.js"
+  },
+  {
+    "revision": "4956e01ca4e89547f52b4bb2114781e0",
+    "url": "/sortly/static/js/2.8de8f01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d9f85dd420f4caa9f7",
+    "url": "/sortly/static/js/main.a9e4d064.chunk.js"
+  },
+  {
+    "revision": "e3881d7bb6ed7247f22b",
+    "url": "/sortly/static/js/runtime-main.1e969d39.js"
+  },
+  {
+    "revision": "1a41ffefc52636f9a8fab7694ed0b77d",
+    "url": "/sortly/static/media/area.1a41ffef.jpg"
+  },
+  {
+    "revision": "04c4aefc779908a778cf860e9f1148f2",
+    "url": "/sortly/static/media/coming-soon.04c4aefc.jpg"
+  },
+  {
+    "revision": "3de73253cb17a25c89b7e219ab945463",
+    "url": "/sortly/static/media/logo.3de73253.png"
+  },
+  {
+    "revision": "b123bcc21f57b6ed17059b6f8bda474c",
+    "url": "/sortly/static/media/population.b123bcc2.jpg"
+  }
+]);
